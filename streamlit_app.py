969303f411fde0de2798352126ee4bc893b086b5
utkarsh-agnihotry <utkarsh.agnihotry@gmail.com>
2023-02-11 19:08:48 +0530

    commiting for indesx changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.multiselect("Pick Some Fruits:",list(my_fruit_list.index))
+my_fruit_list=my_fruit_list.set_index('Fruit')
+streamlit.multiselect("Pick Some Fruits:",list(my_fruit_list.index),['Avocado','Banana'])
 streamlit.dataframe(my_fruit_list)
 
