4773e37e9f4002e813313850b42c28da171c9710
utkarsh-agnihotry <utkarsh.agnihotry@gmail.com>
2023-02-12 17:52:35 +0530

    ok

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick Some Fruits:",list(my_fruit_list.index),['Avocado','Grapefruit'])
-streamlit.dataframe(my_fruit_list)
+fruit_selected=streamlit.multiselect("Pick Some Fruits:",list(my_fruit_list.index),['Avocado','Grapefruit'])
+fruits_to_show=fruit_selected.my_fruit_list.loc[fruit_selected]
+streamlit.dataframe(fruits_to_show)
 
